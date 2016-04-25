8c94b0731f54fd98fc88719fa969ace05d792d1e
Alvaro Paco <alvaropaconeto@gmail.com>
2016-04-25 16:05:30 -0300

    The file pt_BR.js of Language

---
+++ languages/pt_BR.js
@@ -0,0 +1,10 @@
+({
+  'languages': "Idiomas disponíveis:".cyan+"{languages}".yellow,
+  'load': {
+    'loaded': "Tradução carregada: ".cyan+"'{id}'".yellow+", language: ".cyan+"{lang}".yellow,
+    'error': "Erro no carregamento: ".red+"'{id}'".yellow+" '{path}'".yellow+", language: ".red+"{lang}".yellow
+  },
+  'translate': {
+    'error': "Erro na tradução i18n: ".red+"'{node}', {params}".yellow
+  }
+})
