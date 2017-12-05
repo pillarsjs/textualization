8c980b4c972d4b11c4155b5636c7af0ec0a3fb9f
lilxelo <cheloq@gmail.com>
2017-12-05 18:42:27 +0100

    un poco narrado

--- examples/example02/server.js
+++ examples/example02/server.js
@@ -3,9 +3,13 @@ const i18n = require("textualization");
 
 project.services.get("http").configure({port:3001}).start(); 
 
+// En un nombre de nodo, en este caso: vanity, cargamos el contenido del directorio 
+// Donde estarán las hojas de traducciones, que tan sólo es un fichero por idioma, con extensión js.
 i18n.load("vanity","./languages");
+// Declaramos los idiomas disponibles en la aplicación, el primero será el por defecto
 i18n.languages = ["es","en","fr"];
 
+// Agregamos iPath, el cual nos permite traducir también las rutas
 project.routes.add(new Route({
 	iPath: 'vanity.paths.greeting'
 },
